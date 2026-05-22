1a14a3047670913027c489f266ed792d071e802b
AndraXXXus <andrakszusz@gmail.com>
2026-05-22 12:43:11 +0200

    comeOneLetsFixAgain

--- api_gemma.py
+++ api_gemma.py
@@ -255,6 +255,7 @@ for iii in [4,5]:
             if prompt == "ADARULE":
                 model_response = ask_chatgpt(client, model, prompt, requirement, atomic_proposition)
                 print(model_response)
+                model_response = model_response.replace("So the final LTL translation is: ", "")
                 model_response = model_response.replace(".FINISH", "").strip()
             if prompt == "ARTEMIS":    
                 model_response = ask_chatgpt(client, model, prompt, requirement, atomic_proposition)
